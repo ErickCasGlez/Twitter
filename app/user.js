8e48762b7b104e008137eb11b6929759263d5d70
ErickCasGlez <erick.cas.glez@gmail.com>
2022-04-24 22:07:01 -0500

    Creación de clase
    Creación de clase en archivo js y modificación de prueba

--- app/user.js
+++ app/user.js
@@ -1,1 +1,10 @@
-const User = require ('./../app/models/u')
+class User {
+    constructor(id, username, name, bio, dateCreated, lasUpload){
+        this.id = id
+        this.username = username
+        this.name = name
+        this.bio = bio
+        this.dateCreated = dateCreated
+        this.lasUpload = lasUpload
+    }
+}
--- test/models/user.test.js
+++ test/models/user.test.js
@@ -1,6 +1,13 @@
-describe("Unit test fo mission commander class", () => {
-    test('Create a mission commander object', () => {
-      const myMissionCommander = new MissionCommander("Yo")
-      expect(myMissionCommander.name).toBe("Yo");
+const User = require('./../../app/user')
+
+describe("Unit test for User class", () => {
+    test('Create am User Object', () => {
+      const user = new user(1, "eri_gonza", "Erick", "Bio", "dateCreated", "lastUpload")
+      expect(user.id).toBe(1)
+      expect(user.username).toBe("eri_gonza")
+      expect(user.name).toBe("Erick")
+      expect(user.bio).toBe("Bio")
+      expect(user.dateCreated).toBe("dateCreated")
+      expect(user.lastUpload).toBe("lastUpload")
     });
   })
