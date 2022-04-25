c935085134796abe050edd96178942f086179763
ErickCasGlez <erick.cas.glez@gmail.com>
2022-04-24 21:58:02 -0500

    Creación
    Creación y modificación básica

---
+++ test/models/user.test.js
@@ -0,0 +1,6 @@
+describe("Unit test fo mission commander class", () => {
+    test('Create a mission commander object', () => {
+      const myMissionCommander = new MissionCommander("Yo")
+      expect(myMissionCommander.name).toBe("Yo");
+    });
+  })
