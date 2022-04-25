3e08f2ed002e39f6f21eb7ed01be8a13459b6f7c
ErickCasGlez <erick.cas.glez@gmail.com>
2022-04-24 23:32:37 -0500

    setters
    Agregando setters

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -1,11 +1,12 @@
 const User = require('./../../app/models/user')
 
-describe("Add getters", () => {
-    test('Add getters', () => {
+describe("Add setters", () => {
+    test('Add setters', () => {
         const user = new User(1, "eri_gonza", "Erick", "Bio")
-        expect(user.getUsername).toBe("eri_gonza")
-        expect(user.getBio).toBe("Bio")
-        expect(user.getDateCreated).not.toBeUndefined()
-        expect(user.getLastUpload).not.toBeUndefined()
+        user.setUsername = "Erick"
+        expect(user.username).toBe("Erick")
+
+        user.setBio = "New Bio"
+        expect(user.bio).toBe("New Bio")
       });
   })
