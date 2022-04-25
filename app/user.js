be0c1439a01d5269f097a7b893c40650c1854bb4
ErickCasGlez <erick.cas.glez@gmail.com>
2022-04-24 22:17:29 -0500

    Modificación
    Se modifica la clase en js, se elimina el archivo de la clase sin terminación

--- app/user.js
+++ app/user.js
@@ -1,11 +1,11 @@
 class User {
-    constructor(id, username, name, bio, dateCreated, lasUpload){
+    constructor(id, username, name, bio, dateCreated, lastUpload){
         this.id = id
         this.username = username
         this.name = name
         this.bio = bio
         this.dateCreated = dateCreated
-        this.lasUpload = lasUpload
+        this.lastUpload = lastUpload
     }
 }
 
